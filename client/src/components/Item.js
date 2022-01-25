f34609c657f0b628afc481aed7c83e0846446267
Jeremiah Candelaria <jeremiahcandelaria@gmail.com>
2022-01-24 18:49:30 -0800

    complete mvp tasks

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -20,6 +20,35 @@ function Item(props) {
     return <h2>Loading item data...</h2>;
   }
 
+  const handleEdit = () => {
+    // To Do an Edit:
+    // 1. Capture an edit click.
+    // 2. Redirect to update form page.
+    props.history.push(`/item-update/${id}`);
+    // 3. Support editing update form.
+    // 4. Click the update button.
+    // 5. Redirect to the item page we are updating. 
+
+  }
+
+  const handleDelete = () => {
+    axios.delete(`http://localhost:3333/items/${id}`)
+      .then(res => {
+        props.setItems(res.data);
+        // props.deleteItem(id)
+        props.history.push('/item-list');
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+  // To Do a Delete:
+  // 1. Capture a click on the delete button.
+  // 2. Do a delete axios call on the current item.
+  // 3. Update local state to reflect the deleted item.
+  // 4. Redirect to the item list page.
+
+
   return (
     <div className="item-wrapper">
       <div className="item-header">
@@ -46,10 +75,10 @@ function Item(props) {
         path="/item-list/:id/shipping"
         render={props => <ItemShipping {...props} item={item} />}
       />
-      <button className="md-button">
+      <button onClick={handleEdit} className="md-button">
         Edit
       </button>
-      <button className="md-button">
+      <button onClick={handleDelete} className="md-button">
         Delete
       </button>
     </div>
